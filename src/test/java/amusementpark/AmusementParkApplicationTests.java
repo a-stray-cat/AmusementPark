e9d747f4f0a4649801ba77873c414a93d4b98326
a-stary-cat <1191876950@qq.com>
2022-02-25 14:02:11 +0800

    项目初始化

---
+++ src/test/java/amusementpark/AmusementParkApplicationTests.java
@@ -0,0 +1,13 @@
+package amusementpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
